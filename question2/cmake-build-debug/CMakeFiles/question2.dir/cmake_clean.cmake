2d32c221477ef03fc0976c6c6cf224a69e72e70c
TB81266 <xxxxshang@163.com>
2020-06-11 17:50:07 +0800

    first commit

---
+++ question2/cmake-build-debug/CMakeFiles/question2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/question2.dir/main.cpp.o"
+  "question2.pdb"
+  "question2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/question2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
